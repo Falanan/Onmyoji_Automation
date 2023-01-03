5eb00bf55033835df2a3d442410b394d539ba0da
Wenbo Zhang <wenbo.zhang1@ontariotechu.net>
2023-01-02 23:50:10 -0500

    01.01

---
+++ Onmyoji_Automation_Script/main_func.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import cv2 as cv
+import numpy as np
+import pyautogui
+import threading
+import time
+
+
+
+
--- Onmyoji_Automation_Script/test.py
+++ Onmyoji_Automation_Script/test.py
@@ -1,13 +1,55 @@
+import matplotlib.pyplot as plt
+import cv2 as cv
+import numpy as np
+import pyautogui
 import threading
-
-def test_opt(x, y):
-    for i in range(x, y):
-        print(i)
+import time
 
 
-thread1 = threading.Thread(name="T1", target=test_opt, args=(1, 10))
-thread2 = threading.Thread(name="T2", target=test_opt, args=(11, 20))
+
+class getScreenShotThread(threading.Thread):
+    global screenshots_list
+
+    def run(self):
+        # global screenshots_list
+        while True:
+            img_pyautogui = pyautogui.screenshot()
+            # img_pyautogui = cv.cvtColor(np.asarray(img_pyautogui),cv.COLOR_RGB2BGR)
+            # img_pyautogui = np.asanyarray(img_pyautogui)
+            screenshots_list.append(img_pyautogui)
+            time.sleep(5)
+
+class printLengthThread(threading.Thread):
+    global screenshots_list
+
+    def run(self):
+        while True:
+            print(len(screenshots_list))
+            time.sleep(4)
 
 
-thread1.start()
-thread2.start()
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    screenshots_list = []
+
+    sc_thread = getScreenShotThread()
+    prt_thread = printLengthThread()
+
+    sc_thread.start()
+    prt_thread.start()
+
+    # time.sleep(200)
+    # exit(0)
+
+    pass
