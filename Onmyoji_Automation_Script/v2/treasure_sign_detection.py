753f9d0e2d3602082e5375cc198005dbf46e6004
Wenbo Zhang <wenbo.zhang1@ontariotechu.net>
2023-01-06 18:49:46 -0500

    23.01.06 improved

--- Onmyoji_Automation_Script/v2/treasure_sign_detection.py
+++ Onmyoji_Automation_Script/v2/treasure_sign_detection.py
@@ -13,13 +13,12 @@ class treasureSignDetection(threading.Thread):
         super(treasureSignDetection, self).__init__(*args, *kwargs)
         self.img_list = []
         self.methods = ['cv.TM_CCOEFF', 
-                'cv.TM_CCOEFF_NORMED', 
-                'cv.TM_CCORR',
-                'cv.TM_CCORR_NORMED', 
-                'cv.TM_SQDIFF', 
-                'cv.TM_SQDIFF_NORMED']
+                        'cv.TM_CCOEFF_NORMED', 
+                        'cv.TM_CCORR',
+                        'cv.TM_CCORR_NORMED', 
+                        'cv.TM_SQDIFF', 
+                        'cv.TM_SQDIFF_NORMED']
         img_t = cv.imread("Onmyoji_Automation_Script/pics/07-T.png")
-        # img_t = cv.imread("pics/04-T.jpg")
         img_t = cv.cvtColor(img_t, cv.COLOR_BGR2RGB)
         for col in range(0, img_t.shape[1]):
             for row in range(0, img_t.shape[0]):
@@ -29,41 +28,7 @@ class treasureSignDetection(threading.Thread):
         img_t = img_t[10:, :, :]
         img_t = cv.resize(img_t, [128, 128])
         sq_temp_img, nlevels = self.make_square(img_t)
-        self.template_ds = self.gen_down_sample(sq_temp_img, nlevels-4) # generate down sampling for template
-        
-
-
-    def draw_rect(self, I, bbox):
-        '''
-        This function is used to draw the final match box
-        '''
-        I_ = np.copy(I)
-        c = (1.0, 0, 0) if I_.dtype == 'float32' else (255, 0, 0)
-        cv.rectangle(I_, bbox, c, 3)
-        return I_
-
-
-    def draw_rect(self, I, bbox):
-        '''
-        This function is used to draw the final match box
-        '''
-        I_ = np.copy(I)
-        c = (1.0, 0, 0) if I_.dtype == 'float32' else (255, 0, 0)
-        cv.rectangle(I_, bbox, c, 3)
-        return I_
-
-
-    def get_relative_size(self, img_shape):
-        # img_shape = orig_img.shape
-        temp_box_width = img_shape[1] / 672.0
-        width = math.floor(temp_box_width * 197)
-        temp_box_height = img_shape[0] / 465.0
-        height = math.floor(temp_box_height * 130)
-        return width, height
-        
-
-    def setImage(self, img):
-        self.img_list.append(img)
+        self.template_ds = self.gen_down_sample(sq_temp_img, nlevels-2) # generate down sampling for template
 
 
     def highlight(self, R, T, I, use_max=True):
@@ -92,14 +57,51 @@ class treasureSignDetection(threading.Thread):
         c = (1.0, 0, 0) if I_.dtype == 'float32' else (255, 0, 0)
         cv.rectangle(I_, tuple(tl), tuple(br), c, 4)
         return I_, loc, val
+            
 
+    def setImage(self, orig_img):
+        self.img_list.append(orig_img)
+
+    def draw_rect(self, I, bbox):
+        '''
+        This function is used to draw the final match box
+        '''
+        I_ = np.copy(I)
+        c = (1.0, 0, 0) if I_.dtype == 'float32' else (255, 0, 0)
+        cv.rectangle(I_, bbox, c, 3)
+        return I_
+
+    def highlight(self, R, T, I, use_max=True):
+        """
+        Finds the location of maximum (or minimum) matching response, and 
+        draws a rectangle on the image around this location.  The
+        size of the rectangle is determined by template T.
+        
+        Returns an image with the drawn rectangle.  Also returns the loc and
+        the value (maximum or minimum as the case may be).  The original image
+        remains unchanged.
+        """
+        
+        W, H = I.shape[0], I.shape[1]
+        w, h = T.shape[0], T.shape[1]
+        wr, hg = R.shape[0], R.shape[1]
+            
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(R)
+        loc = max_loc if use_max else min_loc
+        val = max_val if use_max else min_val
+        
+        loc1 = loc + np.array([h//2, w//2])               # Size of R is different from I 
+        tl = loc1 - np.array([h//2, w//2])
+        br = loc1 + np.array([h//2, w//2])
+        I_ = np.copy(I)
+        c = (1.0, 0, 0) if I_.dtype == 'float32' else (255, 0, 0)
+        cv.rectangle(I_, tuple(tl), tuple(br), c, 4)
+        return I_, loc, val
+        
 
     def make_square(self, I):
-        '''
-        This function is used to construct a rectangular images to be used in generate gaussian pyramid
-
-        The return value are the square shaped image and the max level can be used to generate gaussian pyramid
-        '''
+    # This function is used to construct a rectangular images to be used in generate gaussian pyramid
+    # The return value are the square shaped image and the max level can be used to generate gaussian pyramid
         h = I.shape[0]
         w = I.shape[1]
         
@@ -116,6 +118,7 @@ class treasureSignDetection(threading.Thread):
 
         return tmp, n_levels
 
+
     def gen_gaussian_pyramid(self, I, levels):
         '''
         This function is used to generate gaussian pyramid
@@ -128,10 +131,11 @@ class treasureSignDetection(threading.Thread):
             gpI.append(G)
         return gpI
 
+
+
     def gen_down_sample(self, I, levels=6):
         '''
         Generate down sample images for input images
-        
         '''
         G = I.copy()
         pI = [G]
@@ -139,6 +143,16 @@ class treasureSignDetection(threading.Thread):
             G = G[::2,::2,:]
             pI.append(G)
         return pI
+
+    def get_relative_size(self, img_shape):
+        # img_shape = orig_img.shape
+        temp_box_width = img_shape[1] / 672.0
+        # width = math.floor(temp_box_width * 197)
+        width = math.floor(temp_box_width * 370)
+        temp_box_height = img_shape[0] / 465.0
+        # height = math.floor(temp_box_height * 130)
+        height = math.floor(temp_box_height * 150)
+        return width, height
 
     def find_highest_match_box(self, bbox_list, img_shape):
         '''
@@ -154,7 +168,6 @@ class treasureSignDetection(threading.Thread):
          - Third, calculate the average in the candidate list, and round up, then the number should be the 
             most possible place where the button at.
         '''
-
         x_axis = []
         y_axis = []
         for item in bbox_list:
@@ -178,24 +191,24 @@ class treasureSignDetection(threading.Thread):
                 y_dict[item] = y_dict[item] + 1
 
         # print("y_dict:", y_dict)
-
+   
         x_list = []
         for key in x_dict.keys():
             tot = 0
-            for i in range(key - 5, key + 5):
+            for i in range(key - 3, key + 3):
                 if i in x_dict:
                     tot = tot + x_dict[i]
-            if tot > 8:
+            if tot >= 3:
                 x_list.append(key)
 
         y_list = []
         for key in y_dict.keys():
             tot = 0
             # print("testNode0:", key)
-            for i in range(key - 5, key + 5):
+            for i in range(key - 3, key + 3):
                 if i in y_dict:
                     tot = tot + y_dict[i]
-            if tot > 8:
+            if tot >= 3:
                 y_list.append(key)
 
         # print("x_list:", x_list)
@@ -221,22 +234,19 @@ class treasureSignDetection(threading.Thread):
 
         # return [int(x_avg), int(y_avg)]
         if x_avg == 0 or y_avg == 0:
-            return [0, 0, 1, 1]
-
-        # if x_avg > img_shape[1] / 3 or y_avg < img_shape[0] / 2:
-        #     # x_avg = 0
-        #     # y_avg = 0
-        #     return [0, 0, 1, 1]
-
-        if x_avg > img_shape[1] / 2 or y_avg < img_shape[0] / 3:
             # x_avg = 0
             # y_avg = 0
+            return[0, 0, 1, 1]
+
+        if x_avg > img_shape[1] / 2 or y_avg < img_shape[0] / 3:
+        # x_avg = 0
+        # y_avg = 0
             return [0, 0, 1, 1]
 
 
         wid, heig = self.get_relative_size(img_shape)        
         return [x_avg, y_avg, wid, heig]
-    
+
 
     def find_sign(self, orig_img):
         '''
@@ -251,74 +261,73 @@ class treasureSignDetection(threading.Thread):
         match_box_list = []
 
         for template in self.template_ds: # match each template in the down sampling list
-            sq_temp_img, nlevels = self.make_square(template)
-            gpt = self.gen_gaussian_pyramid(sq_temp_img, nlevels) # for each down sampled template generate gaussian pyramid
-            for template2 in gpt: # loop through every pyramid image in the list
-                useful_match = []
-                for item in gpI: # for each image in the gaussian pyramid list of original image
-                    if template2.shape[0] <= item.shape[0] and template2.shape[1] <= item.shape[1]:
-                        R_ = cv.matchTemplate(template2, item, eval(self.methods[3])) # match the tamplate
-                        useful_match.append(R_)
+        # for template in gpt: # loop through every pyramid image in the list
+            useful_match = []
+            for item in gpI: # for each image in the gaussian pyramid list of original image
+                if template.shape[0] <= item.shape[0] and template.shape[1] <= item.shape[1]:
+                    R_ = cv.matchTemplate(template, item, eval(self.methods[3])) # match the tamplate
+                    useful_match.append(R_)
 
-                # print(useful_match)
-                R_val = np.empty([len(useful_match), 3])
-                # print(R_val)
+            # print(useful_match)
+            R_val = np.empty([len(useful_match), 3])
+            # print(R_val)
 
-                # highlighted_list = []
-                # for i in range(len(useful_match)):
-                #     R_ = useful_match[i]
-                #     T_ = template
-                #     I_ = gpI[i]
+            highlighted_list = []
+            for i in range(len(useful_match)):
+                R_ = useful_match[i]
+                T_ = template
+                I_ = gpI[i]
 
-                #     H_, loc, val = self.highlight(R_, T_, I_)
-                #     highlighted_list.append(H_)
+                H_, loc, val = self.highlight(R_, T_, I_)
+                highlighted_list.append(H_)
 
-                #     R_val[i, : ] = np.array([val, loc[0], loc[1]])
+                R_val[i, : ] = np.array([val, loc[0], loc[1]])
 
-                for i in range(len(useful_match)):
-                    R_ = useful_match[i]
-                    T_ = template
-                    I_ = gpI[i]
+            # for i in range(len(useful_match)):
+            #     R_ = useful_match[i]
+            #     T_ = template
+            #     I_ = gpI[i]
 
-                    H_, loc, val = self.highlight(R_, T_, I_)
-                    # highlighted_list.append(H_)
+            #     H_, loc, val = self.highlight(R_, T_, I_)
 
-                    R_val[i, : ] = np.array([val, loc[0], loc[1]])
+            #     R_val[i, : ] = np.array([val, loc[0], loc[1]])
 
 
 
-                np.set_printoptions(suppress=True)
-                # print(R_val)
+            np.set_printoptions(suppress=True)
+            # print(R_val)
 
-                highest_match_pos = 0
-                for index in range(0, len(R_val)):
-                    # print(R_val[index])
-                    if R_val[index][0] > R_val[highest_match_pos][0]:
-                        highest_match_pos = index
-                # print(highest_match_pos)
+            highest_match_pos = 0
+            for index in range(0, len(R_val)):
+                # print(R_val[index])
+                if R_val[index][0] > R_val[highest_match_pos][0]:
+                    highest_match_pos = index
+            # print(highest_match_pos)
 
-                highest_match_box = R_val[highest_match_pos]
-                # print(highest_match_box)
+            highest_match_box = R_val[highest_match_pos]
+            # print(highest_match_box)
 
-                highest_match_box[1] = highest_match_box[1] * np.power(2, highest_match_pos)
-                highest_match_box[2] = highest_match_box[2] * np.power(2, highest_match_pos)
-                # print(highest_match_box)
+            highest_match_box[1] = highest_match_box[1] * np.power(2, highest_match_pos)
+            highest_match_box[2] = highest_match_box[2] * np.power(2, highest_match_pos)
+            # print(highest_match_box)
 
 
-                match_box = np.array([highest_match_box[1], highest_match_box[2], template.shape[0], template.shape[1]], dtype=np.int32)
-                # print(match_box)
-                match_box_list.append([match_box[0], match_box[1]])
+            match_box = np.array([highest_match_box[1], highest_match_box[2], template.shape[0], template.shape[1]], dtype=np.int32)
+            # print(match_box)
+            match_box_list.append([match_box[0], match_box[1]])
 
-                # fig, axs = plt.subplots(1, len(highlighted_list), figsize=(15,5))
-                # for idx, ax in enumerate(axs):
-                #     # ax.set_xticks([])
-                #     # ax.set_yticks([])
-                #     ax.imshow(highlighted_list[idx],cmap="gray")
+            # fig, axs = plt.subplots(1, len(highlighted_list), figsize=(15,5))
+            # for idx, ax in enumerate(axs):
+            #     # ax.set_xticks([])
+            #     # ax.set_yticks([])
+            #     ax.imshow(highlighted_list[idx],cmap="gray")
 
         bbox = self.find_highest_match_box(match_box_list, orig_img.shape)
+        # fb = self.draw_rect(orig_img_copy, bbox)
         # print(bbox)
-        # fb = self.draw_rect(orig_img, bbox)
+        # return fb
         return bbox
+        
 
 
     def run(self):
